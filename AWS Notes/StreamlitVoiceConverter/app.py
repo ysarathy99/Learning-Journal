acc6489c6f69b7cc11d3a0327ef5a9a9884a81b5
Yathish Sarathy <ysarathy99@gmail.com>
2023-11-23 23:04:49 -0800

    Streamlit App: Voice Converter

--- AWS Notes/StreamlitVoiceConverter/app.py
+++ AWS Notes/StreamlitVoiceConverter/app.py
@@ -1,21 +1,30 @@
+
+Streamlit App: Voice Converter
+  
 import streamlit as st
 import boto3
 
 from boto3 import Session
 
 # Insert CLI profile name here
-boto_sess = Session(profile_name='boto3user')
-
-s3_resource = boto_sess.resource('s3')
-for bucket in s3_resource.buckets.all():
-    st.write(bucket.name)
+boto_sess = Session(profile_name='user with permissions to runtime')
 
 st.header('Welcome to  Audio Converter streamlit app!')
 
+sagemaker_runtime = boto_sess.client('sagemaker-runtime',region_name='choose-region')
+
+endpoint_name='your_end_point'
+
+def generate_text(audio):
+    payload=audio
+    response = sagemaker_runtime.invoke_endpoint(EndpointName=endpoint_name,ContentType='audio/wav',Body=payload)
+    result=response['Body'].read()
+    text=result
+    return text
 
 st.header("Audio to text")
 audio = st.file_uploader("Choose a file")
 
 if st.button("Run"):
-    generated_text = "Got it"
+    generated_text = generate_text(audio)
     st.write(generated_text)
