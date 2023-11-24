89b681de041135f671e47e34bb2fc780c1aa7249
Yathish Sarathy <ysarathy99@gmail.com>
2023-11-23 22:01:15 -0800

    Streamlit App: Voice Converter

--- AWS Notes/StreamlitVoiceConverter/app.py
+++ AWS Notes/StreamlitVoiceConverter/app.py
@@ -1,5 +1,17 @@
-import steamlit as st
+import streamlit as st
 import boto3
+
+from boto3 import Session
+
+# Insert CLI profile name here
+boto_sess = Session(profile_name='boto3user')
+
+s3_resource = boto_sess.resource('s3')
+for bucket in s3_resource.buckets.all():
+    st.write(bucket.name)
+
+st.header('Welcome to  Audio Converter streamlit app!')
+
 
 st.header("Audio to text")
 audio = st.file_uploader("Choose a file")
